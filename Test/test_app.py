d21861d5b75e83ae0a766de6367d25fec51a04a8
ElPedroLoco <41363567+ElPedroLoco@users.noreply.github.com>
2024-07-08 15:33:03 +0200

    Update test_app.py

--- Test/test_app.py
+++ Test/test_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import pytest
 from flask import Flask, jsonify, request
 
-# Ajouter le chemin relatif du fichier api.py au sys.path pour pouvoir l'importer
+# Ajouter le chemin relatif du fichier app.py au sys.path pour pouvoir l'importer
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # Importer les éléments nécessaires du fichier api.py
@@ -21,7 +21,7 @@ def client():
 # Teste le chargement du modèle de prédiction
 def test_model_loading():
     # Détermine le chemin du fichier contenant le modèle entraîné
-    model_path = os.path.join("model_weights", "model.pkl")
+    model_path = os.path.join("Credit_Score", "model_weights", "model.pkl")
     # Charge le modèle à partir du fichier
     model = joblib.load(model_path)
     # Vérifie que le modèle a été chargé correctement
@@ -30,7 +30,7 @@ def test_model_loading():
 # Teste le chargement du fichier CSV contenant les données de train
 def test_csv_loading():
     # Détermine le chemin du fichier CSV
-    csv_path = os.path.join("model_weights", "df_train.csv")
+    csv_path = os.path.join("Credit_Score", "model_weights", "df_train.csv")
     # Charge le fichier CSV dans un DataFrame pandas
     df = pd.read_csv(csv_path)
     # Vérifie que le DataFrame n'est pas vide
@@ -44,7 +44,7 @@ def test_prediction():
     # Détermine le chemin du répertoire courant
     current_directory = os.path.dirname(os.path.abspath(__file__))
     # Détermine le chemin du fichier CSV contenant les données de test
-    csv_path = os.path.join("model_weights", "df_train.csv")
+    csv_path = os.path.join("Credit_Score", "model_weights", "df_train.csv")
     # Charge le fichier CSV dans un DataFrame pandas
     df = pd.read_csv(csv_path)
     # Prend un échantillon pour la prédiction
