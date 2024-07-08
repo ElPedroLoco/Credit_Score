a1e022e61b981bd0efd8a2e072f7e0d0e6fc65b3
ElPedroLoco <41363567+ElPedroLoco@users.noreply.github.com>
2024-07-08 15:44:49 +0200

    Update test_app.py

--- Test/test_app.py
+++ Test/test_app.py
@@ -21,7 +21,7 @@ def client():
 # Teste le chargement du modèle de prédiction
 def test_model_loading():
     # Détermine le chemin du répertoire courant
-    current_directory = os.path.dirname(os.path.abspath(__file__))
+    current_directory = os.path.dirname(os.path.abspath(__file__),'..')
     # Détermine le chemin du fichier contenant le modèle entraîné
     model_path = os.path.join(current_directory, "model_weights",  "model.pkl")
     # Charge le modèle à partir du fichier
@@ -32,7 +32,7 @@ def test_model_loading():
 # Teste le chargement du fichier CSV contenant les données de train
 def test_csv_loading():
     # Détermine le chemin du répertoire courant
-    current_directory = os.path.dirname(os.path.abspath(__file__))
+    current_directory = os.path.dirname(os.path.abspath(__file__),'..')
     # Détermine le chemin du fichier CSV
     csv_path = os.path.join(current_directory, "model_weights", "df_train.csv")
     # Charge le fichier CSV dans un DataFrame pandas
@@ -46,7 +46,7 @@ def test_prediction():
     import pandas as pd
     from flask import json
     # Détermine le chemin du répertoire courant
-    current_directory = os.path.dirname(os.path.abspath(__file__))
+    current_directory = os.path.dirname(os.path.abspath(__file__),'..')
     # Détermine le chemin du fichier CSV contenant les données de test
     csv_path = os.path.join(current_directory, "model_weights", "df_train.csv")
     # Charge le fichier CSV dans un DataFrame pandas
