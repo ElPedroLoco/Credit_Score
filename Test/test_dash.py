0d3e1ccd37028d07ef905a2859fa824eddf56b06
ElPedroLoco <41363567+ElPedroLoco@users.noreply.github.com>
2024-07-08 16:30:20 +0200

    Update test_dash.py

--- Test/test_dash.py
+++ Test/test_dash.py
@@ -29,7 +29,7 @@ def mocked_st(monkeypatch):
 @pytest.mark.parametrize('mocked_st', [mocked_session_state], indirect=True)
 def test_compute_color(mocked_st):
     # Importez la fonction compute_color à partir du module dashboard
-    from dashboard import compute_color
+    from dash import compute_color
     # Teste la fonction compute_color pour différentes valeurs
     assert compute_color(30) == "green", "Erreur dans la fonction compute_color."
     assert compute_color(50) == "red", "Erreur dans la fonction compute_color."
@@ -37,7 +37,7 @@ def test_compute_color(mocked_st):
 @pytest.mark.parametrize('mocked_st', [mocked_session_state], indirect=True)
 def test_format_value(mocked_st):
     # Importez la fonction format_value à partir du module dashboard
-    from dashboard import format_value
+    from dash import format_value
     # Teste la fonction format_value pour différentes valeurs
     assert format_value(5.67) == 5.67, "Erreur dans la fonction format_value."
     assert format_value(5.00) == 5, "Erreur dans la fonction format_value."
@@ -45,14 +45,14 @@ def test_format_value(mocked_st):
 @pytest.mark.parametrize('mocked_st', [mocked_session_state], indirect=True)
 def test_find_closest_description(mocked_st):
     # Importez les fonctions find_closest_description et definition_features_df à partir du module dashboard
-    from dashboard import find_closest_description, definition_features_df
+    from dash import find_closest_description, definition_features_df
     # Teste la fonction pour trouver la description la plus proche d'un terme donné
     description = find_closest_description("AMT_INCOME_TOTAL", definition_features_df)
     assert description is not None, "Erreur dans la fonction find_closest_description."
 
 def test_get_state(mocked_st):	
     # Importez la fonction get_state à partir du module dashboard
-    from dashboard import get_state
+    from dash import get_state
     state = get_state()
     assert isinstance(state, dict), "La fonction get_state doit renvoyer un dictionnaire."
     assert "data_received" in state, "Le dictionnaire renvoyé par get_state doit contenir la clé 'data_received'."
